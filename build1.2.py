1f6ef77e55aefd9741433fc23ad0e7b0d5b6e651
Swarup143 <santiswarupnayak1@gmail.com>
2026-01-31 20:45:49 +0530

    progressing about variable and datatypes

---
+++ build1.2.py
@@ -0,0 +1,30 @@
+age=25  #you create a varibale - declaration then you assign a value to it -"initialization"
+print(age) #will print the value assigned to variable "age"
+
+#"=" is an assignment operator
+my_integer_var=10
+print('Integer:', my_integer_var)
+
+my_float_var=4.50
+print('Float:', my_float_var)
+
+my_string_var='hello'
+print('String:', my_string_var)
+
+my_boolean_var=True
+print('Boolean:', my_boolean_var)
+
+my_set_var={7,5,8}
+print('Set:', my_set_var)   #set= unordered and unique and curly braces enclose
+
+my_dictionary_var={'name':'Swarup', 'age':'23'}
+print('Dictionary:' , my_dictionary_var)    #dictionary= key value pairs and curly braces enclosed
+
+my_tuple_var=(7, 5, 8)
+print('Tuple:', my_tuple_var)   #tuple= immutable order and enclosed brackets
+
+my_range_var=range(5)
+print('Range:', my_range_var)   #range= a sequence of number, often used in loops
+
+my_list = [22, 'Hello World', 3.14, True]
+print(my_list)      #list= orderred collection of elements and different datatypes

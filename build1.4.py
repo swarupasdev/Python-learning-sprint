9ad1aa7be70b2976ffddaf96ee2e5cb74f589d5f
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-03 13:37:29 +0530

    String practice and understanding

---
+++ build1.4.py
@@ -0,0 +1,24 @@
+#string and string immutability
+#single quote double quote all equal
+my_str_1='hello'
+my_str_2="hello"
+my_str_3="""my name is santiswarup
+i am learning python right now
+it is a cool language"""
+my_str_4='''my name is santiswarup
+i am learning python right now
+it is a cool language'''
+
+"""information_1=but if i will use the 'sep' parameter inside the print() as "sep='\n'" then it will print like that """
+
+
+"""information_2=If you have single qoute in the sentence use double quote to contain the sentence and if double quote in the sentence use single quote to contain and you can use '\' to escape from single or double quote marks like this below"""
+
+my_str_5='mr. san\'s dog is so innocent'
+
+
+print(my_str_1,my_str_2,my_str_3,my_str_4,'\n')
+
+
+print(my_str_1,my_str_2,my_str_3,my_str_4,my_str_5, sep='\n',)
+

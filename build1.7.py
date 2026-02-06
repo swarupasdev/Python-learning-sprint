639322e677abfebbfd7b8d03621ce30ee2bdd8e5
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-06 19:06:02 +0530

    string slicing

--- build1.7.py
+++ build1.7.py
@@ -3,7 +3,8 @@ str1='Hello Swarup'
 #print(str1[0])
 #print(str1[9])
 #print(str1[-1])
-print(str1[2:])
+print(str1[0:12])
 print(str1[-1:])
-
+print(str1[1:10:3])
 #string[start:stop:step]
+#start is inclusive stop is non inclusive

5430e154bf56f9f357c9d4d0a7437191fc4c537a
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-12 18:29:59 +0530

    Augmented assignments work

--- build1.11.py
+++ build1.11.py
@@ -1,10 +1,5 @@
-#numbers and mathematical operations
-#floor ivision (//)
-myint1=56
-myint2=12
-myfloat1=34.6
-myfloat2=5.0
-myfloordiv1=myint1//myint2
-myfloordiv2=myfloat1//myfloat2
-print('Integer floor division:',myfloordiv1)
-print('Float floor division:',myfloordiv2)
+#variable<operator>= value
+#variable=variable<operator>value
+my_var=10
+my_var+=5
+print(my_var)

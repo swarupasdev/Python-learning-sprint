c378608d060d31ebb8a7af06f6909f59f7ed8289
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-12 23:02:57 +0530

    and or not use

---
+++ build1.13.py
@@ -0,0 +1,9 @@
+#and or not operator use in if statement
+is_citizen=True
+age = 18
+
+if is_citizen and age>=18:
+    print("You can vote")
+
+if not is_citizen or age >=18:
+    print("You can vote too")

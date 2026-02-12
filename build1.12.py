933db7a78403164929a27d64e517265480a75fcc
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-12 22:32:10 +0530

    flow control if elif else

---
+++ build1.12.py
@@ -0,0 +1,12 @@
+age=20
+if age>=20:
+    print("You are an adult")
+elif age<=19:
+    print("You are teen")
+elif age<=18:
+    print("You are minor but teen")
+elif age<=12:
+    print("You are a kid")
+else:
+    print("Child")
+

ca7b843d386bfe99add23762b251fbc238281232
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-14 00:01:23 +0530

    Created a small project on mobie ticket booking calculator

---
+++ build1.14.py
@@ -0,0 +1,24 @@
+base_price = 15
+age = 21
+seat_type = 'Gold'
+show_time = 'Evening'
+
+if age > 17:
+    print('User is eligible to book a ticket')
+
+if age >= 21:
+    print('User is eligible for Evening shows')
+else:
+    print('User is not eligible for Evening shows')
+
+is_member = True
+is_weekend = False
+
+discount = 0
+
+if is_member and age>=21:
+    discount = 3
+    print('User qualifies for membership discount')
+else:
+    print('User does not qualify for membership discount')
+print('Discount:', discount)

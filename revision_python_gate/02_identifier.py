88c877c876b604d480a8b9737da3c7aba9acc4e5
swarupasdev <santiswarupnayak1@gmail.com>
2026-05-29 18:19:40 +0530

    example given

--- revision_python_gate/02_identifier.py
+++ revision_python_gate/02_identifier.py
@@ -1,1 +1,11 @@
 #identifier is a name having a few letters , numbers and special characters_
+# python is case-sensitive
+#constant is identifier you can not see the concept of constant in python so you can not define a constant basically ,you have to ,manually think that this variable is a const
+
+count1 = 1
+count2 = 5
+
+print(count1, count2)
+
+count3 = count1 + count2
+print(count3)

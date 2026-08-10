7e4d2ea9127588ce648cbe3d621d7571c197636b
swarupasdev <santiswarupnayak1@gmail.com>
2026-08-10 21:01:42 +0530

    Fix formatting and improve input prompts in 16_arrayinput.py; update while loop logic for emp_id

--- revision_python_gate/09_loops.py
+++ revision_python_gate/09_loops.py
@@ -18,7 +18,7 @@ i = 0
 while True:
     i += 1
     print(i)
-    if(i == 3):
+    if i == 3:
         break
 print("rest of the code")
 
--- revision_python_gate/16_arrayinput.py
+++ revision_python_gate/16_arrayinput.py
@@ -3,4 +3,23 @@ stu_roll = array('i',[])
 n = int(input("Enter number of Elements:"))
 
 for i in range(n):
-    stu_roll.append(int(input("Enter Element:")))
+    stu_roll.append(int(input("Enter Element: ")))
+
+for i in range(len(stu_roll)):
+    print(stu_roll[i])
+
+#using while loop
+
+from array import*
+emp_id = array("i",[])
+m = int(input("Enter number of element: "))
+
+j = 0
+k = 0
+while j<m:
+    emp_id.append(int(input("enter your element: ")))
+    j+=1
+
+while j<len(emp_id):
+    print(emp_id[j])
+    j+=1

c495a44198d0088c58d3e7418f84c5e841e4de52
swarupasdev <santiswarupnayak1@gmail.com>
2026-08-25 12:56:36 +0530

    Refactor 21_reverse.py to print array elements before and after reversing

--- revision_python_gate/21_reverse.py
+++ revision_python_gate/21_reverse.py
@@ -1,4 +1,16 @@
 from array import *
 
 stu_mark = array('i', [40, 50, 60, 70, 80])
-print(stu_mark.reverse())
+n = len(stu_mark)
+i = 0
+while i<n:
+    print(stu_mark[i])
+    i+=1
+print("After array reverse")
+stu_mark.reverse()
+n = len(stu_mark)
+i = 0
+while i<n:
+    print(stu_mark[i])
+    i+=1
+

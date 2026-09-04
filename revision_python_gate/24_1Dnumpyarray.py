168d2ab089220e4878997f9f8370de1301acaf6d
swarupasdev <santiswarupnayak1@gmail.com>
2026-09-04 23:18:39 +0530

    Refactor 1D NumPy array script to print all elements individually

--- revision_python_gate/24_1Dnumpyarray.py
+++ revision_python_gate/24_1Dnumpyarray.py
@@ -2,3 +2,8 @@ import numpy
 stu_roll = numpy.array([101, 102, 103, 104, 105])
 print(stu_roll)
 print(stu_roll.dtype)  #for knowing data type
+print(stu_roll[0])
+print(stu_roll[1])
+print(stu_roll[2])
+print(stu_roll[3])
+print(stu_roll[4])

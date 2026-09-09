50d4fbcf49b3ce9c24df4b4ae1e30fcb6208302e
swarupasdev <santiswarupnayak1@gmail.com>
2026-09-09 23:17:24 +0530

    Refactor printing method in 1D NumPy array script to use index-based iteration

--- revision_python_gate/25_linspace1D.py
+++ revision_python_gate/25_linspace1D.py
@@ -1,3 +1,13 @@
 from numpy import*
 marks = linspace(1,8,5,endpoint = True)
-print(marks)
+
+#print(marks)
+
+#without index
+# for element in marks:
+#         print(element)
+
+#with index
+n = len(marks)
+for i in range(n):
+    print(marks[i])
